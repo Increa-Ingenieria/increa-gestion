2d485c2ea8703cd8bea7965c8b23d17891e3e3f1
Increa-Ingenieria <kikomartinezruiz@gmail.com>
2024-12-30 10:13:16 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+// [Aquí va el código completo del componente que creamos antes]
